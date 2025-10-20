8b40f0bc2aac3dd240a4f6d3db2bdeb6e5be89fb
2399matt <notoriousQR@gmail.com>
2025-10-20 12:00:10 -0400

    Lifetime for explosions, abstract away from game screen

---
+++ core/src/main/java/io/github/tutorial/Main.java
@@ -0,0 +1,37 @@
+package io.github.tutorial;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+
+    public FitViewport viewport;
+
+    public SpriteBatch batch;
+
+    public BitmapFont font;
+
+    @Override
+    public void create() {
+        viewport = new FitViewport(16, 9);
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setUseIntegerPositions(false);
+        font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
+        setScreen(new FirstScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
